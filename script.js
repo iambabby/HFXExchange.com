b5dd92c909e2b3dd902113b4b54d6085fdd547a2
iambabby <154559119+iambabby@users.noreply.github.com>
2023-12-27 15:11:09 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+alert("Welcome to HFX Exchange")
+$(document).on("scroll", function () {
+    var scrollPos = $(document).scrollTop();
+    var navbar = $(".navbar");
+
+    //Add or remove navbar-solid class based on scroll position
+
+    if (scrollPos > 0) {
+        navbar.addClass("navbar-solid");
+    } else {
+        navbar.removeClass("navbar-solid");
+    }
+
+ // Add or remove active class on nav-links
+
+ $(".nav-link").each(function () {
+    var currLink = $(this);
+    var currLink = $(currLink.attr("href"));
+    if (
+        refElement.position().top <= scrollPos &&
+        refElement.position().top + refElement.height() >
+        scrollPos
+    ) {
+        $("nav-link").removeClass("active");
+        currLink.addClass("active");
+    } else {
+       currLink.removeClass("active");
+    }
+  });
+ });
+
+// Mobile Menu
+$(".menu-btn").click(function () {
+    $(".navbar").toggleClass("mobile");
+    $(".menu-btn").toggleClass("active");
+   });
+   
